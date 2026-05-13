af5549fde6254e67fe7f40e8a5784636ff30ac7e
n3tw4lk3r <piroman.pasha@yandex.ru>
2026-05-14 00:35:32 +0300

    Rename left and right to lhs and rhs

--- include/common/value_comparator.hpp
+++ include/common/value_comparator.hpp
@@ -9,15 +9,15 @@ namespace dbms {
 class ValueComparator {
 public:
     static bool compare(
-        const Value& left,
-        const Value& right,
+        const Value& lhs,
+        const Value& rhs,
         const std::string& operator_str
     );
 
     static bool between(
         const Value& value,
-        const Value& left,
-        const Value& right
+        const Value& lhs,
+        const Value& rhs
     );
 };
 
--- src/common/value_comparator.cpp
+++ src/common/value_comparator.cpp
@@ -3,17 +3,17 @@
 namespace dbms {
 
 bool ValueComparator::compare(
-    const Value& a,
-    const Value& b,
+    const Value& lhs,
+    const Value& rhs,
     const std::string& operator_str
 ) {
-    if (a.getType() != b.getType()) {
+    if (lhs.getType() != rhs.getType()) {
         return false;
     }
 
-    if (a.getType() == Value::Type::kInt) {
-        int x = a.asInt();
-        int y = b.asInt();
+    if (lhs.getType() == Value::Type::kInt) {
+        int x = lhs.asInt();
+        int y = rhs.asInt();
 
         if (operator_str == "==") {
             return x == y;
@@ -40,9 +40,9 @@ bool ValueComparator::compare(
         }
     }
 
-    if (a.getType() == Value::Type::kString) {
-        const auto& x = a.asString();
-        const auto& y = b.asString();
+    if (lhs.getType() == Value::Type::kString) {
+        const auto& x = lhs.asString();
+        const auto& y = rhs.asString();
 
         if (operator_str == "==") {
             return x == y;
@@ -74,26 +74,26 @@ bool ValueComparator::compare(
 
 bool ValueComparator::between(
     const Value& value,
-    const Value& left,
-    const Value& right
+    const Value& lhs,
+    const Value& rhs
 ) {
-    if (value.getType() != left.getType() ||
-        value.getType() != right.getType()) {
+    if (value.getType() != lhs.getType() ||
+        value.getType() != rhs.getType()) {
         return false;
     }
 
     if (value.getType() == Value::Type::kInt) {
         int x = value.asInt();
 
-        return left.asInt() <= x &&
-               x < right.asInt();
+        return lhs.asInt() <= x &&
+               x < rhs.asInt();
     }
 
     if (value.getType() == Value::Type::kString) {
         const auto& x = value.asString();
 
-        return left.asString() <= x &&
-               x < right.asString();
+        return lhs.asString() <= x &&
+               x < rhs.asString();
     }
 
     return false;
