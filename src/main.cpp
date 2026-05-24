a21f36ee6648b19da16f36ba56655f23377ae600
n3tw4lk3r <piroman.pasha@yandex.ru>
2026-05-24 16:07:39 +0300

    Implement batch mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <exception>
+#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -7,27 +8,18 @@
 #include "sqlparser/parser.hpp"
 #include "sqlparser/query_buffer.hpp"
 
-int main() {
-    using namespace dbms;
+namespace dbms {
 
-    System system;
-    Parser parser;
-    Executor executor(system);
+void executeQueries(
+    std::istream& input,
+    Parser& parser,
+    Executor& executor
+) {
     QueryBuffer buffer;
-
     std::string line;
-    bool should_exit = false;
 
-    std::cout << "Enter 'exit' to exit." << std::endl;
-
-    while (!should_exit) {
-        if (!std::getline(std::cin, line)) {
-            should_exit = true;
-            break;
-        }
-
+    while (std::getline(input, line)) {
         if (line == "exit") {
-            should_exit = true;
             break;
         }
 
@@ -36,9 +28,42 @@ int main() {
             try {
                 auto cmd = parser.parse(query);
                 executor.execute(cmd);
-            } catch (const std::exception& e) {
-                std::cout << "An error occured: " << e.what() << '\n';
+
+            } catch (const std::exception& error) {
+                std::cerr << error.what() << std::endl;
             }
         }
     }
 }
+
+} // namespace dbms
+
+int main(int argc, char** argv) {
+    using namespace dbms;
+
+    System system;
+    Parser parser;
+    Executor executor(system);
+
+    if (argc == 1) {
+        std::cout << "Enter 'exit' to exit." << std::endl;
+        executeQueries(std::cin, parser, executor);
+        return EXIT_SUCCESS;
+    }
+
+    if (argc == 2) {
+        std::ifstream file(argv[1]);
+
+        if (!file.is_open()) {
+            std::cerr << "Failed to open file" << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        executeQueries(file, parser, executor);
+        return EXIT_SUCCESS;
+    }
+
+    std::cerr << "Usage: ./dbms <script.txt>" << std::endl;
+    return EXIT_FAILURE;
+}
+
